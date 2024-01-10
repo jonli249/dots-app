97d4627cfb50a80c4241f48172a7e7332b569100
Jon Li <jli@Jons-MBP.lan>
2024-01-10 10:24:10 -0800

    IMport

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,7 +2,7 @@
 
 import { SessionProvider } from 'next-auth/react';
 import { AppProps } from 'next/app';
-import './styles/globals.css';
+import '../styles/globals.css';
 
 
 function MyApp({ Component, pageProps }: AppProps) {
