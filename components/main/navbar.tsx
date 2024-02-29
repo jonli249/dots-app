9a205b2138f6249292d3951e50c0ac110e9b20c9
Jon Li <jli@Jons-MBP.lan>
2024-02-28 17:18:06 -0800

    Skrt

--- components/main/navbar.tsx
+++ components/main/navbar.tsx
@@ -9,12 +9,14 @@ function Navbar() {
   const { pathname } = useRouter();
   const showSearchButton = pathname !== '/' && pathname !== '/settings';
 
+  const imageContainerClass = showSearchButton ? "imageContainerWithSearch" : "imageContainerWithoutSearch";
+
   return (
    <div className="px-5 z-[100]">
-
-        <nav className="w-full mx-auto flex items-center justify-between pt-3 sm:pt-8">
+        <nav className="w-full mx-auto flex items-center justify-between pt-2 sm:pt-3">
           <Link href="/">
-            <div style={{ width: '36px', height: '36px' }}>
+            {/* Apply the conditional class or style here */}
+            <div className={imageContainerClass} style={{ width: '36px', height: '36px' }}>
               <Image src="/logo.png" alt="Logo" width={36} height={36} />
             </div>
           </Link>
