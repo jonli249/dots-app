f998f6efbe646f7eae09200bc717987ef146ed9b
deepak9875 <deepaklakhlan1234@gmail.com>
2024-03-10 11:37:15 +0530

    make some change in hero section

--- components/home/Hero.tsx
+++ components/home/Hero.tsx
@@ -4,7 +4,7 @@ import Heroswiper from "./Heroswiper";
 
 const Hero = () => {
   return (
-    <div className="w-full bg-[url(/hero-bg.png)] bg-cover bg-center bg-no-repeat h-[70vh] xl:h-[85vh] 2xl:h-screen flex justify-center items-center">
+    <div className="w-full bg-[url(/img/hero-bg.png)] bg-cover bg-center bg-no-repeat h-[70vh] xl:h-[85vh] 2xl:h-screen flex justify-center items-center">
       <div className="flex flex-col items-center max-w-[900px] w-full">
         <div className="flex gap-6 items-center sm:flex-row flex-col">
           <Image
@@ -18,14 +18,20 @@ const Hero = () => {
             Smart Music Credits for
           </h1>
         </div>
-        <div className="w-full max-w-[350px] sm:max-w-[685px] relative h-fit">
-          <div className="md:max-w-[250px] sm:max-w-[100px] max-w-[50px] w-full absolute left-0 top-9 bg-[linear-gradient(90deg,#fff_8.21%,rgba(0,0,0,0)_82.61%);] z-10 h-[60px] sm:h-[100px]"></div>
+
+        {/* swiper section */}
+        <div className="w-full max-lg:px-3 max-w-[300px] sm:max-w-[540px] lg:max-w-[787px] relative h-fit">
+          {/* white box */}
+          <div className="max-w-[50px] md:max-w-[250px] sm:max-w-[100px] w-full absolute left-0 top-9 bg-[linear-gradient(90deg,#fff_8.21%,rgba(0,0,0,0)_82.61%)] z-10 h-[60px] sm:h-[100px]"></div>
+
           <div className="w-full mt-[51px]">
             <Heroswiper />
           </div>
-          <div className="md:max-w-[250px] sm:max-w-[100px] max-w-[50px] w-full absolute right-0 top-9 bg-[linear-gradient(270deg,#fff_8.21%,rgba(0,0,0,0)_82.61%);] z-10 h-[60px] sm:h-[100px]"></div>
+
+          {/* white box */}
+          <div className="max-w-[50px] md:max-w-[250px] sm:max-w-[100px] w-full absolute right-0 top-9 bg-[linear-gradient(270deg,#fff_8.21%,rgba(0,0,0,0)_82.61%)] z-10 h-[60px] sm:h-[100px]"></div>
         </div>
-        <div className="w-[220px] h-[115px] mt-4 sm:mt-[51px] bg-[url(/btn-bg.png)] bg-center bg-cover bg-no-repeat flex items-center justify-center">
+        <div className="w-[220px] h-[115px] mt-4 sm:mt-[51px] bg-[url(/img/btn-bg.png)] bg-center bg-cover bg-no-repeat flex items-center justify-center">
           <button
             type="submit"
             className="max-w-[180px] sm:max-w-[209px] w-full h-[45px] sm:h-[71px] rounded-[7px] text-white font-inter text-base sm:text-[19px] font-bold hover:bg-transparent hover:text-black bg-black shadow-[2.495px_4.99px_24.951px_0px_rgba(0,0,0,0.25)]"
--- components/home/Heroswiper.tsx
+++ components/home/Heroswiper.tsx
@@ -25,31 +25,27 @@ export default function Heroswiper() {
       }}
       breakpoints={{
         320: {
-          slidesPerView: 3,
           spaceBetween: 10,
         },
         640: {
-          slidesPerView: 4,
           spaceBetween: 100,
         },
         768: {
-          slidesPerView: 4,
           spaceBetween: 10,
         },
         1024: {
-          slidesPerView: 3,
-          spaceBetween: 150,
+          spaceBetween: 10,
         },
       }}
       navigation={true}
       modules={[Autoplay]}
-      className="mySwiper flex gap-4"
+      className="mySwiper"
     >
       {swiperData.map((item, index) => (
         <SwiperSlide key={index}>
           <button
             type="submit"
-            className={`px-4 py-1 lg:py-3 rounded-[10px] w-[100px] sm:w-[165px] lg:w-[255px] h-[48px] sm:h-[52px] lg:h-[74px] bg-[rgba(255,255,255,0.30)] shadow-[2px_ 4px_10px_0px_rgba(0,0,0,0.10)] text-black text-[14px] sm:text-[20px] lg:text-[35px] font-semibold font-inter ${
+            className={`px-2 sm:px-4 py-1 lg:py-3 rounded-[10px] w-[85px] sm:w-[165px] lg:w-[255px] h-[48px] sm:h-[52px] lg:h-[74px] bg-[rgba(255,255,255,0.30)] shadow-[2px_ 4px_10px_0px_rgba(0,0,0,0.10)] text-black text-[14px] sm:text-[20px] lg:text-[35px] font-semibold font-inter ${
               activeSlide === index && "border border-[#CDCDCD]"
             }`}
           >
