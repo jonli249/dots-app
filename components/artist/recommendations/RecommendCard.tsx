150c38fa1e1af72245b30ced19adc147e9a7a419
deepak9875 <deepaklakhlan1234@gmail.com>
2024-03-11 14:17:25 +0530

    make some change

--- components/artist/recommendations/RecommendCard.tsx
+++ components/artist/recommendations/RecommendCard.tsx
@@ -5,9 +5,9 @@ interface RecommendCardProps {
 }
 const RecommendCard: React.FC<RecommendCardProps> = ({ item }) => {
   return (
-    <div className="w-[175px] sm:w-[185px] h-[49px] px-3 border bg-white shadow-[2.445px_4.89px_12.225px_0px_rgba(0,0,0,0.10)] border-black rounded-[75px] flex gap-1 sm:gap-[17px] items-center ">
+    <div className="w-[175px] sm:w-[185px] cursor-pointer hover:border-[#DDD3A0] hover:shadow-[0.832px_1.665px_4.162px_0px_rgba(0,0,0,0.10)] transition duration-300 ease-in-out  h-[49px] px-3 border bg-white shadow-[2.445px_4.89px_12.225px_0px_rgba(0,0,0,0.10)] border-black rounded-[75px] flex gap-1 sm:gap-[17px] items-center ">
       <Image src="/taylor-img.png" width={33} height={33} alt="harry-image" />
-      <p className="text-black whitespace-nowrap text-center font-telegraf text-[14px] sm:text-[18px] font-extrabold">
+      <p className="text-black whitespace-nowrap text-center font-telegraf text-[14px] sm:text-[16px] font-extrabold">
         {item.name}
       </p>
     </div>
